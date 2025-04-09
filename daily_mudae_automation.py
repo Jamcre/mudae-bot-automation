d90bdfd67c294b6c851560ffc0aac8f40ba1f31f
jamcre <jmcrespo123@gmail.com>
2025-04-08 22:33:09 -0400

    GitHub Actions workflow attempt

--- daily_mudae_automation.py
+++ daily_mudae_automation.py
@@ -1,16 +1,35 @@
 # Imports
+import os
 import time
+import chromedriver_autoinstaller
+from dotenv import load_dotenv
 from selenium import webdriver
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By 
 
 # Constants
-DISCORD_EMAIL = "NULL" 
-DISCORD_PASS = "NULL"
-DISCORD_CHANNEL = "NULL"
+load_dotenv()
+DISCORD_EMAIL = os.getenv("DISCORD_EMAIL")
+DISCORD_PASS = os.getenv("DISCORD_PASS")
+DISCORD_CHANNEL = "https://discord.com/channels/756710284298551346/1225299689994321951"
 
-# Start Browser
-driver = webdriver.Chrome()
+# Start Display (for headless)
+display = Display(visible=0, size=(1200, 800))
+display.start()
+
+# Install Chrome driver
+chromedriver_autoinstaller.install()
+
+# Chrome options
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")  # Modern headless mode
+options.add_argument("--window-size=1200,800")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+
+# Create Chrome driver
+driver = webdriver.Chrome(options=options)
 driver.get("https://discord.com/login")
 
 # Discord Log-in
@@ -28,19 +47,21 @@ time.sleep(2) # Page-Loading delay
 driver.get(DISCORD_CHANNEL)
 
 
-# Text Input Automation
+# # Text Input Automation
 time.sleep(2) # Page-Loading delay
 channel_text_field = driver.find_element(by=By.CSS_SELECTOR, value="[aria-label='Message #mudae-s3']")
 channel_text_field.send_keys('$tu')
 channel_text_field.send_keys(Keys.ENTER)
-time.sleep(2) # Page-Loading delay
-channel_text_field.send_keys('$daily')
-channel_text_field.send_keys(Keys.ENTER)
-time.sleep(2) # Page-Loading delay
-channel_text_field.send_keys('$dk')
-channel_text_field.send_keys(Keys.ENTER)
+# time.sleep(2) # Page-Loading delay
+# channel_text_field.send_keys('$daily')
+# channel_text_field.send_keys(Keys.ENTER)
+# time.sleep(2) # Page-Loading delay
+# channel_text_field.send_keys('$dk')
+# channel_text_field.send_keys(Keys.ENTER)
 
 # Close Browser
+time.sleep(2) # Page-Loading delay
+print('Mudae Automation Complete')
 driver.quit()
 
 
